1181f9d896469f377645ec1279f035222aaa3131
slavkolukic <58329051+slavkolukic@users.noreply.github.com>
2020-03-23 22:14:12 +0100

    Added initial design

---
+++ vs-project/Grafika-Projekat1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grafika_Projekat1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static bool isFullscreen = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TopBarGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void FullScreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isFullscreen)
+            {
+                WindowState = WindowState.Normal;
+                isFullscreen = false;
+                fullScreenButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#F18C8E"));
+                fullScreenButton.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#305F72"));
+                fullScreenButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Fullscreen;
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                isFullscreen = true;
+                fullScreenButton.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#F18C8E"));
+                fullScreenButton.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#305F72"));
+                fullScreenButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.FullscreenExit;
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
