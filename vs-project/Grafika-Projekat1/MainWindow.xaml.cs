4f8edb94003af55b0d1d32101e5393796fa1fead
slavkolukic <58329051+slavkolukic@users.noreply.github.com>
2020-03-24 17:58:00 +0100

    Functional toggle buttons

--- vs-project/Grafika-Projekat1/MainWindow.xaml.cs
+++ vs-project/Grafika-Projekat1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,10 +22,21 @@ namespace Grafika_Projekat1
     public partial class MainWindow : Window
     {
         private static bool isFullscreen = false;
+        private static string activeShape = "none";
+        private static List<Button> buttons = new List<Button>();
+        private static List<PackIcon> icons = new List<PackIcon>();
 
         public MainWindow()
         {
             InitializeComponent();
+            buttons.Add(elipseBtn);
+            buttons.Add(rectangleBtn);
+            buttons.Add(polygonBtn);
+            buttons.Add(imageBtn);
+            icons.Add(elipseBtnIcon);
+            icons.Add(rectangleBtnIcon);
+            icons.Add(polygonBtnIcon);
+            icons.Add(imageBtnIcon);
         }
 
         private void TopBarGrid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -60,6 +72,69 @@ namespace Grafika_Projekat1
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        private void TurnOffButtons()
+        {
+            foreach (var btn in buttons)
+            {
+                if (btn.Name.Contains(activeShape))
+                    btn.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#D8737F"));
+                else
+                    btn.BorderBrush = new SolidColorBrush(Colors.LightGray);
+            }
+
+            foreach (var icon in icons)
+            {
+                if (icon.Name.Contains(activeShape))
+                    icon.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#D8737F"));
+                else
+                    icon.Foreground = new SolidColorBrush(Colors.LightGray);
+            }
+        }
+
+        private void ElipseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeShape == "elipse")
+                activeShapeLbl.Content = "none";
+            else
+                activeShapeLbl.Content = "elipse";
+
+            activeShape = activeShapeLbl.Content.ToString();
+            TurnOffButtons();
+        }
+
+        private void RectangleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeShape == "rectangle")
+                activeShapeLbl.Content = "none";
+            else
+                activeShapeLbl.Content = "rectangle";
+
+            activeShape = activeShapeLbl.Content.ToString();
+            TurnOffButtons();
+        }
+
+        private void PolygonBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeShape == "polygon")
+                activeShapeLbl.Content = "none";
+            else
+                activeShapeLbl.Content = "polygon";
+
+            activeShape = activeShapeLbl.Content.ToString();
+            TurnOffButtons();
+        }
+
+        private void ImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeShape == "image")
+                activeShapeLbl.Content = "none";
+            else
+                activeShapeLbl.Content = "image";
+
+            activeShape = activeShapeLbl.Content.ToString();
+            TurnOffButtons();
         }
     }
 }
